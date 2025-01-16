113b972860030abc046b6ddd6302cfb4819f5049
Cody Seibert <webdevcody@gmail.com>
2025-01-15 22:38:25 -0500

    minor tweaks

---
+++ packages/game-server/src/config/index.ts
@@ -0,0 +1,7 @@
+export const FPS = 30;
+
+export const DAY_DURATION = 30;
+export const NIGHT_DURATION = 60;
+
+export const PERFORMANCE_LOG_INTERVAL = 5000;
+export const TICK_RATE_MS = 1000 / FPS;
