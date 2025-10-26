65d8d283e93d384f62115790217ed8be090f7f17
Cody Seibert <webdevcody@gmail.com>
2025-10-25 22:29:13 -0400

    update routes configuration by commenting out the unused play route; prepare for future route adjustments

--- packages/website/app/routes.ts
+++ packages/website/app/routes.ts
@@ -1,12 +1,12 @@
 import { type RouteConfig } from "@react-router/dev/routes";
 
 export default [
+  // {
+  //   path: "/",
+  //   file: "routes/index.tsx",
+  // },
   {
     path: "/",
-    file: "routes/index.tsx",
-  },
-  {
-    path: "/play",
     file: "routes/play.tsx",
   },
   {
